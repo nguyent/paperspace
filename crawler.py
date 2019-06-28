dd59eb6b92a2a4f40c2101e7535178317ac18e3c
Thang Nguyen <thang@thangnguyen.us>
2019-06-28 11:01:50 -0400

    get imports from a file with nodevisitor

--- crawler.py
+++ crawler.py
@@ -16,11 +16,38 @@ def getAllPythonFiles(path):
 
 loc = '/Users/thang/work/paperspace/repos/numpy'
 
-f = '/Users/thang/work/paperspace/repos/numpy/runtests.py'
-
-# An abs file path
 def getAST(filePath):
     with open(filePath) as f:
         return ast.parse(f.read())
 
-print(getAST(f))
+
+# via: https://docs.python.org/3/library/ast.html#ast.NodeVisitor
+
+class ImportNodeVisitor(ast.NodeVisitor):
+    def __init__(self):
+        self.imports = {
+            'import': [],
+            'from': []
+        }
+
+    def visit_Import(self, node):
+        self.imports['import'].extend(node.names)
+        self.generic_visit(node)
+
+    def visit_ImportFrom(self, node):
+        self.imports['from'].extend(node.names)
+        self.generic_visit(node)
+
+    def getImports(self):
+        return self.imports
+
+def main():
+    f = '/Users/thang/work/paperspace/repos/numpy/runtests.py'
+    tree = getAST(f)
+
+    visitor = ImportNodeVisitor()
+    visitor.visit(tree)
+    print(visitor.getImports())
+
+if __name__ == '__main__':
+    main()
